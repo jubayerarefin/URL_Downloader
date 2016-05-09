800fdff5464535cc492350ab0f25af5effc3a8b1
Jubayer Arefin <dipu2005@gmail.com>
2016-05-09 17:07:47 +0600

    Completed Parser 0.0.1

--- Parser.py
+++ Parser.py
@@ -1,8 +1,9 @@
 import urllib
 import sys
 
-__name__ = 'Parser'
+# __name__ = 'Parser'
 __author__ = 'Jubayer Arefin'
+__email__ = 'dipu2005 at gmail.com'
 
 
 class Parser(object):
@@ -18,25 +19,33 @@ class Parser(object):
 
     # Download parsed urls and save to disk
     def download(self, argv):
-        print argv
-        if not argv:
+        if argv:
             if len(argv) > 1:
-                print "Pass only one text file name."
+                print "Pass only one text file name as parameter."
                 exit()
             elif 0 < len(argv) < 2:
                 self.container = argv[0]
+                print 'I will now try to download the contents of this file: ' + argv[0]
         opener = urllib.URLopener()
         urls = self.parse()
         for url in urls:
             # Strip of spaces if there is any
             url.strip(' ')
             # Check if the line of text starts with `http://` or `https://`
-            if url.startswith('http://', 0, 7) or url.startswith('https://', 0, 8):
+            print 'Checking url(s)...\n'
+            if url == "\n" or url is None:
+                print 'url(s) are not valid.'
+                exit()
+            elif url.startswith('http://', 0, 7) or url.startswith('https://', 0, 8):
                 print url.split('/')[-1]
-                # opener.retrieve(url, url.split('/')[-1])
+                opener.retrieve(url, url.split('/')[-1])
+                print "is downloaded"
+            else:
+                print "url(s) are not valid. Please provide valid url that starts with http: or https:"
 
 
-parser = Parser()
-sys.argv.append('asdasd')
-sys.argv.append('dlist.txt')
-parser.download(sys.argv[1:])
+if __name__ == '__main__':
+    parser = Parser()
+    parser.download(sys.argv[1:])
+else:
+    print 'Sorry, can\'t run'
